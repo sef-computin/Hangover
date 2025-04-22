5e3f470b782a370b05ce469e74abecc4445c05e2
sef-computin <74347726+sef-computin@users.noreply.github.com>
2025-04-22 14:45:48 +0400

    events db mock and tests

--- src/events/go.mod
+++ src/events/go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/google/uuid v1.6.0
 	github.com/lib/pq v1.10.9
+	github.com/magiconair/properties v1.8.10
+	go.uber.org/mock v0.5.1
 )
 
 require (
@@ -19,6 +21,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -29,10 +32,10 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
-	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ src/events/handlers/handlers_test.go
@@ -0,0 +1,64 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/magiconair/properties/assert"
+	"github.com/sef-comp/Hangover/events/dbhandler/mock_dbhandler"
+	"github.com/sef-comp/Hangover/events/models"
+	"go.uber.org/mock/gomock"
+)
+
+func SetUpRouter() *gin.Engine {
+	router := gin.Default()
+	return router
+}
+
+func TestGetAllFlights(t *testing.T) {
+	gin.SetMode(gin.ReleaseMode)
+
+	type mockBehaviour func(r *mock_dbhandler.MockEventDB, record []*models.Event)
+	tests := []struct {
+		name                 string
+		output               []*models.Event
+		username             string
+		mockBehaviour        mockBehaviour
+		expectedStatusCode   int
+		expectedResponseBody string
+	}{
+		{
+			name: "Ok",
+			mockBehaviour: func(r *mock_dbhandler.MockEventDB, output []*models.Event) {
+				r.EXPECT().GetAllEvents().Return(output, nil)
+			},
+			expectedStatusCode: http.StatusOK,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			c := gomock.NewController(t)
+			defer c.Finish()
+
+			var events []*models.Event
+			dbhandler := mock_dbhandler.NewMockEventDB(c)
+			test.mockBehaviour(dbhandler, events)
+
+			handler := EventHandler{DBHandler: dbhandler}
+
+			r := gin.New()
+			r.GET("/events", handler.GetAllEventsHandler)
+
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest("GET", "/flights", nil)
+			req.Header.Set("X-User-Name", test.username)
+
+			r.ServeHTTP(w, req)
+
+			assert.Equal(t, w.Code, test.expectedStatusCode)
+		})
+	}
+}
